0c3fc77fdbc7018c1048a4f2b4ad08b25217d18c
Parth Mangukiya <paradoxinfotechltd@gmail.com>
2025-05-20 10:11:36 +0530

    fix: Update path to api_services in SUPPORT class

--- config/_SUPPORT.php
+++ config/_SUPPORT.php
@@ -148,7 +148,7 @@ class SUPPORT
 		}
 
 		$msg = "";
-		$path = dirname(__DIR__, 1) . "/api_services/";
+		$path = ROOT_URL . "/api_services/";
 		try {
 			if (is_array($__msg)) {
 				$msg = print_r($__msg, true);
